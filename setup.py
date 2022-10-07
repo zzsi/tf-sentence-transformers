605ea6df35bcaa270dcf198a29e79c407c8e1d1a
ZZ <zz@kungfu.ai>
2022-10-07 14:46:17 -0500

    adjust ci

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(name='tf_sentence_transformers',
       version='0.1',
       description='A Tensorflow SentenceTransformer layer that takes in strings as input.',
@@ -8,9 +11,8 @@ setup(name='tf_sentence_transformers',
       author_email='zhangzhang.si@gmail.com',
       license='MIT',
       packages=['tf_sentence_transformers'],
-      install_requires=[
-          'tensorflow>=2.0.0',
-          'transformers'
-      ],
+      python_requires=">=3.6",
+      install_requires=install_requires,
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False
+    )
