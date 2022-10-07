160ca96ca716050c4fe38256f97be40f2641fae7
ZZ <zz@kungfu.ai>
2022-10-07 14:51:37 -0500

    from_pt=False

--- tests/test_sentence_transformer.py
+++ tests/test_sentence_transformer.py
@@ -1,7 +1,10 @@
 from tf_sentence_transformers import SentenceTransformer
 
+
 def test_call():
-    layer = SentenceTransformer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+    layer = SentenceTransformer.from_pretrained(
+        "sentence-transformers/all-MiniLM-L6-v2", from_pt=False
+    )
     inputs = [
         ["This is a test sentence."],
         ["This is another test sentence."],
--- tf_sentence_transformers/__init__.py
+++ tf_sentence_transformers/__init__.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 from tensorflow import keras
 from transformers import AutoTokenizer, TFAutoModel
@@ -15,13 +14,16 @@ class SentenceTransformer(tf.keras.layers.Layer):
     print(embedding.shape)
     ```
     """
+
     def __init__(self, tokenizer, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.tokenizer = tokenizer
         self.model = model
-    
+
     @classmethod
-    def from_pretrained(cls, model_name: str="sentence-transformers/all-MiniLM-L6-v2", from_pt=True):
+    def from_pretrained(
+        cls, model_name: str = "sentence-transformers/all-MiniLM-L6-v2", from_pt=False
+    ):
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         pretrained_setr_tf = TFAutoModel.from_pretrained(model_name, from_pt=from_pt)
         return cls(tokenizer=tokenizer, model=pretrained_setr_tf)
@@ -29,28 +31,48 @@ class SentenceTransformer(tf.keras.layers.Layer):
     def tf_encode(self, inputs):
         def encode(inputs):
             inputs = [x[0].decode("utf-8") for x in inputs.numpy()]
-            outputs = self.tokenizer(inputs, padding=True, truncation=True, return_tensors='tf')
-            return outputs['input_ids'], outputs['token_type_ids'], outputs['attention_mask']
-        return tf.py_function(func=encode, inp=[inputs], Tout=[tf.int32, tf.int32, tf.int32])
-    
+            outputs = self.tokenizer(
+                inputs, padding=True, truncation=True, return_tensors="tf"
+            )
+            return (
+                outputs["input_ids"],
+                outputs["token_type_ids"],
+                outputs["attention_mask"],
+            )
+
+        return tf.py_function(
+            func=encode, inp=[inputs], Tout=[tf.int32, tf.int32, tf.int32]
+        )
+
     def process(self, i, t, a):
-      def __call(i, t, a):
-        model_output = self.model({'input_ids': i.numpy(), 'token_type_ids': t.numpy(), 'attention_mask': a.numpy()})
-        return model_output[0]
-      return tf.py_function(func=__call, inp=[i, t, a], Tout=[tf.float32])
+        def __call(i, t, a):
+            model_output = self.model(
+                {
+                    "input_ids": i.numpy(),
+                    "token_type_ids": t.numpy(),
+                    "attention_mask": a.numpy(),
+                }
+            )
+            return model_output[0]
+
+        return tf.py_function(func=__call, inp=[i, t, a], Tout=[tf.float32])
 
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = tf.squeeze(tf.stack(model_output), axis=0)
         input_mask_expanded = tf.cast(
-            tf.broadcast_to(tf.expand_dims(attention_mask, -1), tf.shape(token_embeddings)),
-            tf.float32
+            tf.broadcast_to(
+                tf.expand_dims(attention_mask, -1), tf.shape(token_embeddings)
+            ),
+            tf.float32,
         )
         a = tf.math.reduce_sum(token_embeddings * input_mask_expanded, axis=1)
-        b = tf.clip_by_value(tf.math.reduce_sum(input_mask_expanded, axis=1), 1e-9, tf.float32.max)
+        b = tf.clip_by_value(
+            tf.math.reduce_sum(input_mask_expanded, axis=1), 1e-9, tf.float32.max
+        )
         embeddings = a / b
         embeddings, _ = tf.linalg.normalize(embeddings, 2, axis=1)
         return embeddings
-      
+
     def call(self, inputs):
         input_ids, token_type_ids, attention_mask = self.tf_encode(inputs)
         model_output = self.process(input_ids, token_type_ids, attention_mask)
@@ -58,7 +80,7 @@ class SentenceTransformer(tf.keras.layers.Layer):
         embedding_dim = model_output[0].shape[-1]
         embeddings = tf.reshape(embeddings, (-1, embedding_dim))
         return embeddings
-    
+
 
 # If using sentence-transformers (torch):
 # setr = SentenceTransformer('paraphrase-MiniLM-L6-v2')
